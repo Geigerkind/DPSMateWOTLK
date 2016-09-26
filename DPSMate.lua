d0f6d3c0b71319686b34656ee1d39ca9a4d2251f
Albea <tomdymel@web.de>
2016-09-26 10:37:55 +0200

    Fixed german loot parsing issue

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 20
+DPSMate.VERSION = 21
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
