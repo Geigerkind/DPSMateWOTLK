e62bca51d42172d68ad6851a481b3b250f5ba9cb
Albea <tomdymel@web.de>
2016-09-29 15:57:01 +0200

    Fixed german whisper

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 21
+DPSMate.VERSION = 22
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
