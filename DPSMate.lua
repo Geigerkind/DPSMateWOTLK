9e48a784b186005fe2543cf0066b82c9679fd6f9
Tom Dy <tomdymel@web.de>
2016-12-16 12:24:40 +0100

    wrong name issue

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 10
+DPSMate.VERSION = 11
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
