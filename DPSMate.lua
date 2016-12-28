b0aacb4e3f62913f447f980b847d236853075d40
Albea <tomdymel@web.de>
2016-12-28 15:55:58 +0100

    MBB support

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 12
+DPSMate.VERSION = 13
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
