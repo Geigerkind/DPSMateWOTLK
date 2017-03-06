8a316dcfa6336b157eef4f2511f1ead3f6dd1129
Albea <tomdymel@web.de>
2017-03-06 09:51:42 +0100

    Lua error hotfix

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 18
+DPSMate.VERSION = 19
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
